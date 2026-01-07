0e38fdca1a85fca8a2632845f3a7f380ffbc983d
Sevalino Elfata <sevalinoelfata8@gmail.com>
2026-01-07 13:38:49 +0700

    update new github contact

--- src/data/contacts.js
+++ src/data/contacts.js
@@ -3,7 +3,7 @@ import linkedin from '../assets/icons/linkedin.svg'
 import instagram from '../assets/icons/instagram.svg'
 
 export const contactsData = [
-    { id: 1, name: "Github", link: "https://github.com/ELfatt09", icon: github },
+    { id: 1, name: "Github", link: "https://github.com/sevelfatt", icon: github },
     { id: 2, name: "Linkedin", link: "https://www.linkedin.com/in/sevalino-elfata-31755030a/", icon: linkedin },
     { id: 3, name: "Instagram", link: "https://www.instagram.com/sevelfatt.dev/", icon: instagram },
 ]
